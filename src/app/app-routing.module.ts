f4f7c5b057e1616fbbf5701560c9e6a75bcf8dd3
nada.h <nada.h22425@gmail.com>
2022-03-26 19:04:37 +0200

    responsive header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "header", component: HeaderComponent  },
+  // { path: "hello", component: HeaderComponent, data: { routeName: "Hello2" } }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+}
+
+// routes.forEach((eachRoute) => {
+//   RouteNames.routeNamesObject[eachRoute.data.routeName] = eachRoute.path;    // now all route paths are added to this prop
+// })
+//  const routeNames = RouteNames;
